093fb541206a3eb71b1f8edd3bf700ea725fcf11
Mont <mmasri@amazon.com>
2020-03-21 14:13:53 +0200

    changes

--- src/app/homepage/loggedin/logged-in.component.ts
+++ src/app/homepage/loggedin/logged-in.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {QuestionComponentComponent} from '../../question-component/question-component.component';
-import {QuestionsComponentComponent} from '../../questions-component/questions-component.component';
 
 @Component({
   selector: 'app-user-homepage',
